a70bbcef60bc94031a8322810e7aed4f127f3f52
jialiang.feng <gaaloeng@163.com>
2019-11-01 18:42:54 +0800

    add "GenericMethodExample" add some explanation

--- src/generic/daoexample/DAO.java
+++ src/generic/daoexample/DAO.java
@@ -1,5 +1,6 @@
 package generic.daoexample;
 
+//表的共性操作DAO
 public class DAO<T> {
 
     //增
@@ -13,4 +14,12 @@ public class DAO<T> {
 
     //改
     public void modify(T data){ }
+
+    //泛型方法
+    //举例: 可能有"获取表中所有记录总数"的需求;
+    //     可能有"获取最大的员工收入"的需求
+    //则返回的值就不是这个ORM类了,是另外具体的类
+    public <E> E getSomeSpecialValue(){
+        return null;
+    }
 }
--- src/generic/daoexample/StudentDao.java
+++ src/generic/daoexample/StudentDao.java
@@ -1,5 +1,6 @@
 package generic.daoexample;
 
+//只能操作一个表的具体DAO操作
 public class StudentDao extends DAO<Student> {
 }
 
